ead0ba0e9aed7a4e78297358cfb158171ce324b0
lohith <lohith05amal@gmail.com>
2022-02-16 19:19:22 -0500

    routes defined

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const app = express();
 
 // connect database
 connectDB();
+// define routes
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/profiles", require("./routes/api/profiles"));
+app.use("/api/post", require("./routes/api/post"));
+app.use("/api/auth", require("./routes/api/auth"));
 
 app.get("/", (req, res) => res.send("API RUNNING"));
 const PORT = process.env.PORT || 5000;
